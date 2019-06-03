e322eb0d5a62d98fe4134737f3c3ac3ead14468b
Abhishek Indoria <indoriaabhishek@gmail.com>
2019-06-02 18:47:49 -0700

    Pre-Layout Change Draft

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,7 +6,18 @@ class Landing extends Component {
     return (
       <div className="Landing">
         Landing Page
-        <div className="Landing__Main" />
+        <div className="Landing__Main">
+          <img src="https://picsum.photos/400/300" />
+          <div class="Landing__Main__Content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+        </div>
         <div className="Landing__Side_1" />
         <div className="Landing__Side_2" />
         <div className="Landing__News" />
