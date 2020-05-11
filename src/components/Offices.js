9d13daf4c3d7e299e9b4af309fedb714042f10d3
Abhishek Indoria <indoriaabhishek@gmail.com>
2020-05-10 22:46:54 -0700

    Add Consul and Dictator cards, Complete card formatting

--- src/components/Offices.js
+++ src/components/Offices.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import "./styles/Offices.css";
 import Offices_Sidebar from "./Offices_sidebar.js";
 import Offices_Card from "./Offices_card.js";
-import Offices_Card_2 from "./Offices_card_2.js";
+import Dictator_Sulla from "./images/Dictator_Sulla.jpg";
+import Consul_Cicero from "./images/Consul_Cicero.jpg";
 import {
   BrowserRouter as Router,
   Switch,
@@ -21,13 +22,28 @@ class Offices extends Component {
             <div className="Offices__Title-text">
               Senatorial Offices of the Republic of Rome
             </div>
-            <div className="Offices__Title-subtext">[placeholder]</div>
           </div>
           <Offices_Sidebar></Offices_Sidebar>
           <div className="Offices__Landing__Content">
             <Switch>
-              <Route path="/offices/dictator" component={Offices_Card}></Route>
-              <Route path="/offices/consul" component={Offices_Card_2} />
+              <Route path="/offices/dictator">
+                <Offices_Card
+                  officeHeading="VACANT"
+                  officeSubHeading="Last Held By: SYLLA"
+                  officePicture={Dictator_Sulla}
+                  officeDescription="A consul or (in emergency situations) praetor can appoint a dictator; this proposal can not be vetoed. Rome's first dictator was Aulus Postumius Albinus, who was appointed in the first decade of the fifth century BCE, when the Latin allies revolted. "
+                  officeDescriptionSub="A dictatorship lasts 6 months and has to be renewed upon the approval of the Senate. A dictator(Master of the infantry: MAGISTER POPVLI) often appoints a Master of the horse, MAGISTER EQVITVM."
+                ></Offices_Card>
+              </Route>
+              <Route path="/offices/consul">
+                <Offices_Card
+                  officeHeading="M•TVLLIVS•CICERO"
+                  officeSubHeading="Co-Consul: C•M•F•Antonius"
+                  officePicture={Consul_Cicero}
+                  officeDescription="Two men are elected by the Comitia centuriata, one of the three voting assemblies of the people. The consuls serve for only one year (to prevent corruption) and can only rule when they agree, because each consul can veto the other one's decision."
+                  officeDescriptionSub="They also command the Roman army (both have two legions) and exercise the highest juridical power in the Roman empire."
+                ></Offices_Card>
+              </Route>
             </Switch>
           </div>
         </div>
--- src/components/Offices_card.js
+++ src/components/Offices_card.js
@@ -4,7 +4,23 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class Offices_Card extends Component {
   render() {
-    return <div className="Offices_Card">SALVE</div>;
+    return (
+      <div className="Offices_Card">
+        <div className="Offices_Card-picture">
+          <img src={this.props.officePicture}></img>
+        </div>
+        <div className="Offices_Card-text-container">
+          <div className="Offices_Card-heading">
+            <div>{this.props.officeHeading}</div>
+            <div>{this.props.officeSubHeading}</div>
+          </div>
+          <div className="Offices_Card-description">
+            <p>{this.props.officeDescription}</p>
+            <p>{this.props.officeDescriptionSub}</p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
