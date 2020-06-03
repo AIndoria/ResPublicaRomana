d6bfc2fc41294cf7543314b253698a7e0812592a
Abhishek Indoria <indoriaabhishek@gmail.com>
2020-06-03 02:25:22 -0700

    Updates

---
+++ precache-manifest.d9e049d40df5c45476a052a458c8106a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b1700033bd6f9d8eab8fa9a749f12e",
+    "url": "/ResPublicaRomana/index.html"
+  },
+  {
+    "revision": "76ee7d27974d74b423ad",
+    "url": "/ResPublicaRomana/static/css/main.8de6af9c.chunk.css"
+  },
+  {
+    "revision": "e92e29401b4fae650521",
+    "url": "/ResPublicaRomana/static/js/2.792e9923.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ResPublicaRomana/static/js/2.792e9923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76ee7d27974d74b423ad",
+    "url": "/ResPublicaRomana/static/js/main.433dd02c.chunk.js"
+  },
+  {
+    "revision": "56c43ebcd51276b0f41f",
+    "url": "/ResPublicaRomana/static/js/runtime-main.b1460764.js"
+  },
+  {
+    "revision": "38d43efd3551cc5e471bcb43bbed1913",
+    "url": "/ResPublicaRomana/static/media/Consul_Cicero.38d43efd.jpg"
+  },
+  {
+    "revision": "8c5a0a46bcb34cb48b12945549007412",
+    "url": "/ResPublicaRomana/static/media/Dictator_Sulla.8c5a0a46.jpg"
+  },
+  {
+    "revision": "a5ed59173fb27b6b49726a053569e2ba",
+    "url": "/ResPublicaRomana/static/media/SPQR.a5ed5917.svg"
+  },
+  {
+    "revision": "dd2ba92fec4de5aef12994e4ebb8d1d1",
+    "url": "/ResPublicaRomana/static/media/art.dd2ba92f.jpg"
+  },
+  {
+    "revision": "3188aa99c4e001845ed462598b180061",
+    "url": "/ResPublicaRomana/static/media/capitalina.3188aa99.jpg"
+  },
+  {
+    "revision": "56bd35c884de4fa017d24138ac5479af",
+    "url": "/ResPublicaRomana/static/media/cato.56bd35c8.jpg"
+  },
+  {
+    "revision": "f5f03999fcf6870eae685cf7e3eb74c9",
+    "url": "/ResPublicaRomana/static/media/for_barbarians.f5f03999.jpg"
+  },
+  {
+    "revision": "ffbcab6e195d0f1f799ac085eb5e1af5",
+    "url": "/ResPublicaRomana/static/media/for_romans.ffbcab6e.jpg"
+  },
+  {
+    "revision": "f5be3bca47e3e3c8f6d9466f669e2e95",
+    "url": "/ResPublicaRomana/static/media/militari.f5be3bca.jpg"
+  },
+  {
+    "revision": "87fdac88d97c98128cff6a0e5ecad4bc",
+    "url": "/ResPublicaRomana/static/media/senate.87fdac88.jpg"
+  }
+]);
